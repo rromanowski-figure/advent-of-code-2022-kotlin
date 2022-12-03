4d3e4d317e40b1dc563903e0b4b192b0d45cf7ee
Richard Romanowski <79664656+rromanowski-figure@users.noreply.github.com>
2022-12-03 08:54:57 -0500

    solved 03.1 and 03.2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,39 @@
 fun main() {
+    fun Char.priority() = code - if (this.isUpperCase()) 38 else 96
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val priorities = input.map { contents ->
+            val half = contents.length / 2
+            val left = contents.substring(0, half)
+            val right = contents.substring(half, contents.length)
+            val overlap = left.toCharArray().intersect(right.toCharArray().toSet()).single()
+
+            overlap.priority()
+        }
+
+        return priorities.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val groups = input.chunked(3)
+        val priorities = groups.map {
+            val overlap = it[0].toCharArray()
+                .intersect(it[1].toCharArray().toSet())
+                .intersect(it[2].toCharArray().toSet())
+                .single()
+
+            overlap.priority()
+        }
+
+        return priorities.sum()
     }
 
     val sampleInput = readInput("input-03-sample")
     val sampleOutput1 = part1(sampleInput)
-    check(sampleOutput1 == 0)
+    check(sampleOutput1 == 157)
 
     val sampleOutput2 = part2(sampleInput)
-    check(sampleOutput2 == 0)
+    check(sampleOutput2 == 70)
 
     val input = readInput("input-03")
     println("Part 1: ${part1(input)}")
