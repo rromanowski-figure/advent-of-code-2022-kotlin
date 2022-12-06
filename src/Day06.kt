3be5c423daee821568706705014a6b9b0c9e9ce9
Richard Romanowski <79664656+rromanowski-figure@users.noreply.github.com>
2022-12-06 08:09:53 -0500

    solved day 06

---
+++ src/Day06.kt
@@ -0,0 +1,10 @@
+object Day06 : Runner<Int>(6, 7, 19) {
+    override fun part1(input: List<String>) = uniqueWindex(input.first(), 4)
+
+    override fun part2(input: List<String>) = uniqueWindex(input.first(), 14)
+
+    private fun uniqueWindex(signal: String, size: Int): Int {
+        return signal.windowed(size)
+            .indexOfFirst { it.toSet().size == size } + size
+    }
+}
--- src/Runner.kt
+++ src/Runner.kt
@@ -1,17 +1,21 @@
-abstract class Runner<T>(private val day: Int, private val check1: T, private val check2: T) {
+sealed class Runner<T>(private val day: Int, private val check1: T, private val check2: T) {
     abstract fun part1(input: List<String>): T
     abstract fun part2(input: List<String>): T
 
     fun run() {
         val sampleInput = readSampleInput(day)
         val sampleOutput1 = part1(sampleInput)
-        check(sampleOutput1 == check1)
+        check(sampleOutput1 == check1) {
+            "Sample input for part 1 is wrong. Expected: $check1, Actual: $sampleOutput1"
+        }
 
         val input = readInput(day)
         println("Part 1: ${part1(input)}")
 
         val sampleOutput2 = part2(sampleInput)
-        check(sampleOutput2 == check2)
+        check(sampleOutput2 == check2) {
+            "Sample input for part 2 is wrong. Expected: $check2, Actual: $sampleOutput2"
+        }
 
         println("Part 2: ${part2(input)}")
     }
